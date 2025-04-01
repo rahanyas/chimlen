9b0d7b6ff637d7b5cd667a5c79c0e5224c369f22
rahanyas <rahanyas3@gmail.com>
2025-04-01 14:26:49 +0530

    dubugging token

--- server/Routes/oAuth.Routes.js
+++ server/Routes/oAuth.Routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 import passport from 'passport';
 import generateToken from '../utils/createToken.js';
 import url from 'url'
+import { log } from 'console';
 
 router.get('/google', passport.authenticate('google', {
   scope : ["profile", "email"]
@@ -17,6 +18,9 @@ router.get('/google/callback', passport.authenticate('google', {
       console.log('Error : ', q.error);   
     }
     generateToken(req.user, res);
+    const token = req.cookies.token
+    console.log("token : ", token);
+    
     // put frontend url here
     const redirectUrl = process.env.NODE_ENV === "development" ? "http://localhost:5173/"  : "https://chimlen.vercel.app/"
     console.log('redirect url:',redirectUrl);
