228fbcb3ae8de93747439132220294c10dc870d7
rahanyas <rahanyas3@gmail.com>
2025-04-23 21:30:11 +0530

    redirected user when google login to home page instead of to landinPage, and changing the ui of home page

--- server/Routes/oAuth.Routes.js
+++ server/Routes/oAuth.Routes.js
@@ -21,7 +21,7 @@ router.get('/google/callback', passport.authenticate('google', {
     console.log("generated token : ", req.cookies.token);
     
     // put frontend url here
-    const redirectUrl = process.env.NODE_ENV === "development" ? "http://localhost:5173/"  : "https://chimlen.vercel.app/"
+    const redirectUrl = process.env.NODE_ENV === "development" ? "http://localhost:5173/home"  : "https://chimlen.vercel.app/home"
     console.log('redirect url:',redirectUrl);
     return res.redirect(redirectUrl)
     
