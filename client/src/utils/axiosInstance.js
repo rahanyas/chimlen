d082f971114a7b25ffeaeaee46c40a0dfbd2de8d
rahanyas <rahanyas3@gmail.com>
2025-07-19 11:53:25 +0530

    changed the base url from ui to serv

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_NODE_ENV === "development" ? `${import.meta.env.VITE_BACKEND_URL || 'http://localhost:9000'}/api` : `${import.meta.env.VITE_PRODUCTION_BACKEND_URL || 'https://chimlen-main.vercel.app'}/api`;
+const baseURL = import.meta.env.VITE_NODE_ENV === "development" ? `${import.meta.env.VITE_BACKEND_URL || 'http://localhost:9000'}/api` : `${import.meta.env.VITE_PRODUCTION_BACKEND_URL || 'https://chimlen.onrender.com'}/api`;
 
  console.log('baseurl fo axios : ',baseURL);
 
